2575a4613dceff8fb0dc2a04171798049cdc369d
drewbot <drewbotka@gmail.com>
2016-01-05 19:43:02 -0500

    added templating, customized to use express-handlebars

--- I1_InstallingExpress/app.js
+++ I1_InstallingExpress/app.js
@@ -1,5 +1,15 @@
 var express = require('express');
+var exphbs  = require('express-handlebars');
+
 var app = express();
+
+var hbs = exphbs.create({
+    // Specify helpers which are only registered on this instance.
+    helpers: {
+        foo: function () { return 'FOO!'; },
+        bar: function () { return 'BAR!'; }
+    }
+})
 
 // you can use environment variables
 // (variables declared outside of your app, perhaps on the hosting server's environment)
@@ -13,6 +23,9 @@ var port = process.env.PORT || 3000;
 
 app.use('/assets', express.static(__dirname + '/public'));
 
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
 app.use('/', function(req, res, next) {
   console.log('Request URL: ' + req.url);
   // next method says to move on to the next middleware
@@ -22,18 +35,25 @@ app.use('/', function(req, res, next) {
 // express method for recieving get methods to the '/' route and providing a response
 // express knows to format accordingly
 app.get('/', function(req, res) {
+  // render the handlebars template at index.handlebars
   // when the request for this page is made the markup below will be read and another request wil be made for the stylesheet at /assets/styles.css
-  res.send('<html><head><link href=assets/styles.css type=text/css rel=stylesheet /></head><body><h1>Hello Lovey</h1></body></html>');
+  res.render('home', {
+    showTitle: true,
+    // Override `foo` helper only for this rendering.
+    helpers: {
+        foo: function () { return 'foo.'; }
+    }
+  });
 })
 
 // setup generic person route
 app.get('/person', function(req, res) {
-  res.send('<html><head></head><body><h1>Hello IDless Person</h1></body></html>');
+  res.render('person', {id: 'No ID'});
 })
 
 // person/id route will use matching to take any given sub-route (id passed) and apply to the id variable on req.params
 app.get('/person/:id', function(req, res) {
-  res.send('<html><head></head><body><h1>Person: ' + req.params.id + '</h1></body></html>');
+  res.render('person', {id: req.params.id});
 })
 
 app.get('/api', function(req, res) {
